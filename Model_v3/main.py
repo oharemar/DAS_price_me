33017206288ebc31f7438f4a836456fa83b1a01e
Miroslav Kubu <34068909+kubumiro@users.noreply.github.com>
2020-01-17 11:04:39 +0800

    Add files via upload

---
+++ Model_v3/main.py
@@ -0,0 +1,19 @@
+import numpy as np
+from train import trainModel
+
+input_path = "data/final_dataset.csv"
+target_path = "data/salary_final.csv"
+
+test_input_path = "data/final_dataset.csv"
+test_target_path = "data/salary_final.csv"
+
+param_grid = {
+              'reg__max_depth': np.arange(4, 16, 4),
+              'reg__n_estimators': np.arange(20, 100, 20)
+              }
+
+# Model names:
+#   - XGBRegressor ...............'XGBoost'
+#   - RandomForestRegressor ......'RandomForest'
+trainModel(input_path, target_path, test_input_path, test_target_path, grid=param_grid, model_name='XGBoost',
+           min_cut=100, max_cut=700000, cv=4, eval=True)
